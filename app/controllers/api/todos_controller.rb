8058d86d520e1a9341fd38daf9575d0d064add47
App Academy Student <student@appacademy.io>
2017-09-12 14:51:55 -0700

    Finish Phase 1 Todo rd 2

---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,40 @@
+class Api::TodosController < ApplicationController
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def index
+    render json: Todo.all
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if update_attributes(@todo)
+      render json: @todo
+    else
+      render json: @todo.errors.full_messages, status: 402
+    end
+  end
+
+  def destroy
+    @todo = Todo.find(params[:id])
+    @todo.delete
+    render :index
+  end
+
+  private
+
+  def todo_params
+    require(params).permit(:title, :body, :done)
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+t = Todo.create(title: "Title", body: "Body", done: false)
+u = Todo.create(title: "Damian Lillard", body: "Dame D.O.L.L.A.", done: true)
+v = Todo.create(title: "CJ McCollum", body: "3J", done: true)
+w = Todo.create(title: "Jusuf Nurkic", body: "Big Nurk", done: false)
+x = Todo.create(title: "Al-Farouq Aminu", body: "The Chief", done: false)
